b42f0d2dce3ab6100abe2682e735b3ed0dc609c9
triston1228 <122327698+triston1228@users.noreply.github.com>
2024-05-16 18:40:16 -0400

    Update UIVisualPatches.cs
    ** Ashlands Update

--- ABearCodes.Valheim.CraftingWithContainers/UI/UIVisualPatches.cs
+++ ABearCodes.Valheim.CraftingWithContainers/UI/UIVisualPatches.cs
@@ -1,8 +1,11 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using ABearCodes.Valheim.CraftingWithContainers.Common;
+using ABearCodes.Valheim.CraftingWithContainers.Inventoring;
 using ABearCodes.Valheim.CraftingWithContainers.Tracking;
 using HarmonyLib;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,65 +14,63 @@ namespace ABearCodes.Valheim.CraftingWithContainers.UI
     [HarmonyPatch]
     public class UIVisualPatches
     {
+        [HarmonyPatch(typeof(InventoryGui), "SetupRequirement", typeof(Transform), typeof(Piece.Requirement), typeof(Player), typeof(bool), typeof(int))]
         [HarmonyPrefix]
-        [HarmonyPatch(typeof(InventoryGui), "SetupRequirement",
-            typeof(Transform), typeof(Piece.Requirement),
-            typeof(Player), typeof(bool), typeof(int))]
-        private static bool SetupRequirementTotalItemsIndicatorPatch(Transform elementRoot,
-            Piece.Requirement req, Player player, bool craft, int quality, ref bool __result)
+        private static bool SetupRequirementTotalItemsIndicatorPatch(Transform elementRoot, Piece.Requirement req, Player player, bool craft, int quality, ref bool __result)
         {
-            if (!Plugin.Settings.CraftingWithContainersEnabled.Value ||
-                !Plugin.Settings.ModifyItemCountIndicator.Value)
+            if (!Plugin.Settings.CraftingWithContainersEnabled.Value || !Plugin.Settings.ModifyItemCountIndicator.Value)
                 return true;
-            var component1 = elementRoot.transform.Find("res_icon").GetComponent<Image>();
-            var component2 = elementRoot.transform.Find("res_name").GetComponent<Text>();
-            var component3 = elementRoot.transform.Find("res_amount").GetComponent<Text>();
-            var component4 = elementRoot.GetComponent<UITooltip>();
-            if (req.m_resItem == null) return true;
-            component1.gameObject.SetActive(true);
-            component2.gameObject.SetActive(true);
-            component3.gameObject.SetActive(true);
-            component1.sprite = req.m_resItem.m_itemData.GetIcon();
-            component1.color = Color.white;
-            component4.m_text = Localization.instance.Localize(req.m_resItem.m_itemData.m_shared.m_name);
-            component2.text = Localization.instance.Localize(req.m_resItem.m_itemData.m_shared.m_name);
-            var num = player.GetInventory().CountItems(req.m_resItem.m_itemData.m_shared.m_name);
-            var amount = req.GetAmount(quality);
-            if (amount <= 0)
+
+            if (elementRoot == null || req == null || player == null) return true;
+
+            var iconImage = elementRoot.transform.Find("res_icon")?.GetComponent<Image>();
+            var nameText = elementRoot.transform.Find("res_name")?.GetComponent<TMP_Text>();
+            var amountText = elementRoot.transform.Find("res_amount")?.GetComponent<TMP_Text>();
+            var tooltip = elementRoot.GetComponent<UITooltip>();
+
+            if (iconImage == null || nameText == null || amountText == null || tooltip == null || req.m_resItem == null) return true;
+
+            iconImage.gameObject.SetActive(true);
+            nameText.gameObject.SetActive(true);
+            amountText.gameObject.SetActive(true);
+
+            var itemData = req.m_resItem.m_itemData;
+            iconImage.sprite = itemData.GetIcon();
+            iconImage.color = Color.white;
+            tooltip.m_text = Localization.instance.Localize(itemData.m_shared.m_name);
+            nameText.text = Localization.instance.Localize(itemData.m_shared.m_name);
+
+            // Get the combined item count using the patched CountItems method
+            var totalItemCount = player.GetInventory().CountItems(itemData.m_shared.m_name, quality, craft);
+
+            // Log the total item count
+            Plugin.Log.LogInfo($"Total item count for {itemData.m_shared.m_name}: {totalItemCount}");
+
+            // Get the amount required from Piece.Requirement
+            var amountRequired = req.GetAmount(quality);
+
+            // Log amount required
+            Plugin.Log.LogInfo($"Amount required for {itemData.m_shared.m_name}: {amountRequired}");
+
+            if (amountRequired <= 0)
             {
                 InventoryGui.HideRequirement(elementRoot);
                 __result = false;
                 return false;
             }
 
-            component3.text = string.Format($"{amount}/{num}");
-            if (num < amount)
-                component3.color = (double) Mathf.Sin(Time.time * 10f) > 0.0 ? Color.red : Color.white;
-            else
-                component3.color = Color.white;
+            // Update the amount text with total item count and amount required
+            amountText.text = string.Format($"{totalItemCount}/{amountRequired}");
+            amountText.color = totalItemCount < amountRequired ? (Mathf.Sin(Time.time * 10f) > 0.0 ? Color.red : Color.white) : Color.white;
+
             __result = true;
             return false;
+
+
         }
 
-        [HarmonyPatch(typeof(Hud), "SetupPieceInfo", typeof(Piece))]
-        private static void Postfix(Piece piece, Text ___m_buildSelection)
-        {
-            if (!Plugin.Settings.CraftingWithContainersEnabled.Value || piece == null ||
-                piece.m_name == "$piece_repair") return;
-            var containers = ContainerTracker.GetViableContainersInRangeForPlayer(Player.m_localPlayer,
-                Plugin.Settings.ContainerLookupRange.Value);
-            var maxBuilds = piece.m_resources.Select(resource =>
-            {
-                var playerCount = Player.m_localPlayer.GetInventory()
-                    .CountItemsOriginal(resource.m_resItem.m_itemData.m_shared.m_name);
-                var containerCount = containers.Sum(container =>
-                    container.Container.GetInventory()
-                        .CountItemsOriginal(resource.m_resItem.m_itemData.m_shared.m_name));
-                return (playerCount + containerCount) / resource.m_amount;
-            }).AddItem(Int32.MaxValue)
-            .Min();
-            ___m_buildSelection.text =
-                $"{Localization.instance.Localize(piece.m_name)} (<color=white>{(maxBuilds == int.MaxValue? "∞" : maxBuilds.ToString())}</color>)";
-        }
-    }
+
+    } 
+
+
 }
